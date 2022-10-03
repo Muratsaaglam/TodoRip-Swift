b10ac57ee65f614cad32dca9015c823312c019e5
Muratsaaglam <muratsaaglam@outlook.com>
2022-10-03 22:17:50 +0300

    Initial Commit

---
+++ TodoRip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoRip
+//
+//  Created by Murat Sağlam on 3.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
